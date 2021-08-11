5e51f21f8e287da0162d52869d49fcba32872072
vikiyaputra <viki.yaputra@gmail.com>
2021-08-12 01:36:02 +1000

    #1

--- seeders/20210726044818-add_admin_to_database.js
+++ seeders/20210726044818-add_admin_to_database.js
@@ -1,31 +1,29 @@
 'use strict';
-const user = require(`./admin.json`)
+// const user = require(`./admin.json`)
 
-user.forEach(user =>{
-  user.createdAt = new Date ()
-  user.updatedAt = new Date ()
-})
-
-// [ kalau error pas seeding heroku, pake ini ajah.
-//   {
-//     "email": "admin@mail.com",
-//     "password": "$2a$10$qV0GNxRHvNMTjNZp9A/IF.eYQHIvcWTrISZUqk9j4o0p7IQe0ClWC",
-//     "role": "admin",
-//     "createdAt": "2021-04-23T18:25:43.511Z",
-//     "updatedAt": "2021-04-23T18:25:43.511Z"
-//   },
-//   {
-//     "email": "wrong@mail.com",
-//     "password": "$2a$10$qV0GNxRHvNMTjNZp9A/IF.eYQHIvcWTrISZUqk9j4o0p7IQe0ClWC",
-//     "role": "customer",
-//     "createdAt": "2021-04-23T18:25:43.511Z",
-//     "updatedAt": "2021-04-23T18:25:43.511Z"
-//   }
-// ]
+// user.forEach(user =>{
+//   user.createdAt = new Date ()
+//   user.updatedAt = new Date ()
+// })
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Users', user, {})
+    await queryInterface.bulkInsert('Users', [
+      {
+        "email": "admin@mail.com",
+        "password": "$2a$10$qV0GNxRHvNMTjNZp9A/IF.eYQHIvcWTrISZUqk9j4o0p7IQe0ClWC",
+        "role": "admin",
+        "createdAt": "2021-04-23T18:25:43.511Z",
+        "updatedAt": "2021-04-23T18:25:43.511Z"
+      },
+      {
+        "email": "wrong@mail.com",
+        "password": "$2a$10$qV0GNxRHvNMTjNZp9A/IF.eYQHIvcWTrISZUqk9j4o0p7IQe0ClWC",
+        "role": "customer",
+        "createdAt": "2021-04-23T18:25:43.511Z",
+        "updatedAt": "2021-04-23T18:25:43.511Z"
+      }
+    ], {})
     /**
      * Add seed commands here.
      *
